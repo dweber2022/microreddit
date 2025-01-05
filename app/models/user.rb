080da0401c208d16cc66404d0bae9c94d6b32421
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-04 19:42:23 -0600

    add User model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, length: { in: 3..24 }
+  validates :name, :email, :password, presence: true
+  validates :name, :email, uniqueness: true
+  validates :email, length: { maximum: 320 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { in: 8..64 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true, unless: Proc.new { |a| a.password.blank? }
+end
