4500ecb2b1c3301f08ff7a5e596a9a582b245464
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-04 20:07:05 -0600

    add Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  validates :link, presence: true
+  validates :link, format: { with: URI.regexp([ "http", "https" ]) }
+  validates :link, length: { maximum: 300 }
   belongs_to :user
 end
